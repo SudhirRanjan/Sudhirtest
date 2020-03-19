d9ad3ce07b1709c98c5695cff1b888a6770565ab
sudhir <sudhir.ranjan101@gmail.com>
2020-03-19 18:57:17 +0530

    Edited Msg

--- SudhirTest/src/com/app/SudhirPro.java
+++ SudhirTest/src/com/app/SudhirPro.java
@@ -2,6 +2,24 @@ package com.app;
 
 public class SudhirPro {
 				private int i;
+				public int getI() {
+					return i;
+				}
+				public void setI(int i) {
+					this.i = i;
+				}
+				public String getName() {
+					return name;
+				}
+				public void setName(String name) {
+					this.name = name;
+				}
+				public String getAddress() {
+					return address;
+				}
+				public void setAddress(String address) {
+					this.address = address;
+				}
 				private String name;
 				private String address;
 }
